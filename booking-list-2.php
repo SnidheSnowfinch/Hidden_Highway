bd38507f9d5eb788f77d656f61637b2b6e74c85a
snidhedinesh <snidhe2016@gmail.com>
2025-04-25 18:54:46 +0530

    new-commit

---
+++ booking-list-2.php
@@ -0,0 +1,101 @@
+<?php include 'includes/header.php' ?>
+<div class="home-top-section-inner ">
+    <div class="home-top-content">
+
+        <h1>Booking List</h1>
+        <p>HOME <i class="bi bi-chevron-double-right mx-2"></i>BOOKING LIST</p>
+    </div>
+</div>
+<div class=" w-100">
+
+    <div class="container ">
+
+
+        <div class="homes-section">
+            <div class="booking-card">
+                <div class="left-section">
+                    <img src="bootstrap/images/home-img1.webp" alt="House" class="booking-property-image">
+
+                </div>
+                <div class="middle-section">
+                    <div class="booking-property-info">
+                        <div class="booking-property-name">Hidden Tower Estate</div>
+                        <div class="booking-property-location">Kochi</div>
+                    </div>
+                    <div>
+                        <div class="date-item">
+                            <span class="dot green"></span>
+                            <span>03-oct-24</span>
+                        </div>
+                        <div class="date-item">
+                            <span class="dot red"></span>
+                            <span>04-oct-24</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-section">
+                    <button class="reschedule">RESCHEDULE</button>
+                    <button class="cancel">CANCEL</button>
+                </div>
+            </div>
+            <div class="booking-card">
+                <div class="left-section">
+                    <img src="bootstrap/images/home-img1.webp" alt="House" class="booking-property-image">
+
+                </div>
+                <div class="middle-section">
+                    <div class="booking-property-info">
+                        <div class="booking-property-name">Hidden Tower Estate</div>
+                        <div class="booking-property-location">Kochi</div>
+                    </div>
+                    <div>
+                        <div class="date-item">
+                            <span class="dot green"></span>
+                            <span>03-oct-24</span>
+                        </div>
+                        <div class="date-item">
+                            <span class="dot red"></span>
+                            <span>04-oct-24</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-section">
+                    <button class="reschedule">RESCHEDULE</button>
+                    <button class="cancel">CANCEL</button>
+                </div>
+            </div>
+            <div class="booking-card">
+                <div class="left-section">
+                    <img src="bootstrap/images/home-img1.webp" alt="House" class="booking-property-image">
+
+                </div>
+                <div class="middle-section">
+                    <div class="booking-property-info">
+                        <div class="booking-property-name">Hidden Tower Estate</div>
+                        <div class="booking-property-location">Kochi</div>
+                    </div>
+                    <div>
+                        <div class="date-item">
+                            <span class="dot green"></span>
+                            <span>03-oct-24</span>
+                        </div>
+                        <div class="date-item">
+                            <span class="dot red"></span>
+                            <span>04-oct-24</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-section">
+                    <button class="reschedule">RESCHEDULE</button>
+                    <button class="cancel">CANCEL</button>
+                </div>
+            </div>
+        </div>
+
+    
+
+
+    </div>
+
+</div>
+<?php include 'includes/footer.php' ?>
---
+++ booking-list.php
@@ -0,0 +1,77 @@
+<?php include 'includes/header.php' ?>
+<div class="home-top-section-inner ">
+        <div class="home-top-content">
+
+            <h1>Booking List</h1>
+            <p>HOME <i class="bi bi-chevron-double-right mx-2"></i>BOOKING LIST</p>
+        </div>
+    </div>
+    <div class=" w-100">
+
+        <div class="container ">
+
+
+            <div class="homes-section">
+                
+                <div class="table-container">
+    <table>
+      <thead>
+        <tr>
+          <th class="text-left">Date</th>
+          <th>Property Name</th>
+          <th>Location</th>
+          <th>Update</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Row 1 -->
+        <tr>
+          <td class="text-left">04-oct-24</td>
+          <td class="property">
+          <img src="bootstrap/images/home-img1.webp" alt="House">
+                      <div>Hidden Tower Estate</div>
+          </td>
+          <td>Kochi, Kerala</td>
+          <td >
+            <div class="table-buttons">
+            <button class="reschedule">RESCHEDULE</button>
+            <button class="cancel">CANCEL</button></div>
+          </td>
+        </tr>
+
+        <!-- Row 2 -->
+        <tr>
+          <td class="text-left">04-oct-24</td>
+          <td class="property">
+          <img src="bootstrap/images/home-img1.webp" alt="House">
+                      <div>Hidden Tower Estate</div>
+          </td>
+          <td>Kochi, Kerala</td>
+          <td >
+            <div class="table-buttons">
+            <button class="reschedule">RESCHEDULE</button>
+            <button class="cancel">CANCEL</button></div>
+          </td>
+        </tr>
+
+        <!-- Row 3 -->
+        <tr>
+          <td class="text-left">04-oct-24</td>
+          <td class="property">
+          <img src="bootstrap/images/home-img1.webp" alt="House">
+                      <div>Hidden Tower Estate</div>
+          </td>
+          <td>Kochi, Kerala</td>
+          <td >
+            <div class="table-buttons">
+            <button class="reschedule">RESCHEDULE</button>
+            <button class="cancel">CANCEL</button></div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+            </div>
+        </div>
+    </div>
+<?php include 'includes/footer.php' ?>
